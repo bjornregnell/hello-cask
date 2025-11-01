b465b42543eb9be3136a9d9a188a47ac5734deeb
bjornregnell <bjorn.regnell@cs.lth.se>
2025-11-01 16:49:34 +0100

    add terminal output

--- hello-cask.scala
+++ hello-cask.scala
@@ -4,14 +4,18 @@
 object MinimalApplication extends cask.MainRoutes{
   @cask.get("/")
   def hello() = {
+    println("/ requested -- serving hello world")
     "Hello World!"
   }
 
   @cask.post("/do-thing")
   def doThing(request: cask.Request) = {
-    request.text().reverse
+    val response = request.text().reverse
+    println(s"/$request POST requested-- serving reversed: $response")
+    response
   }
 
   initialize()
   println("hello-cask is running at http://localhost:8080")
+  println("""try "curl -d 'some data' http://localhost:8080/do-thing" """)
 }
